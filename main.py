38aaa9012f1a90f48241eb535102b175a1aa4674
Coder202090803 <ptmastudent09@gmail.com>
2025-09-17 23:02:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -26,10 +26,10 @@ load_dotenv()
 keep_alive()
 
 API_TOKEN = os.getenv("API_TOKEN")
-CHANNELS = [-1002168256708]
-LINKS = ["https://t.me/anilord_1"]
-MAIN_CHANNELS = [-1002168256708]
-MAIN_LINKS = ["https://t.me/anilord_1"]
+CHANNELS = []
+LINKS = []
+MAIN_CHANNELS = []
+MAIN_LINKS = []
 BOT_USERNAME = os.getenv("BOT_USERNAME")
 
 bot = Bot(token=API_TOKEN)
